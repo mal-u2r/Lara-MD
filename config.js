b387e215995dd1985ba476bfdf2c997d0df99139
mal-u2r <malvindarley@gmail.com>
2024-12-28 07:10:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XjYzyR7J#rmt88CfRTlgXMvxGPvOY-DyD0wzCdIRETh5PXSkyvAI", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "vjhQTCZA#hnBEWrmsEA0Em8dQahhm5K-to0wAV4Lm8AMpc2XQGr4", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
